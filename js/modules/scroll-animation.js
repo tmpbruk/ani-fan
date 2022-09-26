f708c94b25a7f7df1ec2d66e7cbbaf31c0ca7444
Tiago Pereira <tmpbruk@gmail.com>
2022-09-26 15:44:49 -0300

    refactor into class

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -6,23 +6,42 @@ export default class AnimationScroll {
     this.halfScreen = window.innerHeight * 0.6;
 
     //Bind
-    this.scrollAnimation = this.scrollAnimation.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  scrollAnimation() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - this.halfScreen < 0;
-      if (isSectionVisible) {
-        section.classList.add("ativo");
+  // Get distance of each item in relation to the top of the website
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.halfScreen),
+      };
+    });
+  }
+
+  // verify distance of each object in relation to website scroll
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add("ativo");
+      } else if (item.element.classList.contains("ativo")) {
+        item.element.classList.add("ativo");
       }
     });
   }
 
   init() {
     if (this.sections.length) {
-      this.scrollAnimation();
-      window.addEventListener("scroll", this.scrollAnimation);
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
     }
+    return this;
+  }
+
+  // remove event from scroll
+  stop() {
+    window.removeEventListener("scroll", this.checkDistance);
   }
 }
